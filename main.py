a4ab5ab8ed5b99c01d30ed3c55044ded9c9ce509
jayanaka-98 <jayanaka15@gmail.com>
2023-11-14 22:45:42 +0530

    Creating tilemaps form images

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ class Game:
         # self.font = pygame.font.Font('Arial', 32)
         self.running = True
 
+        self.character_spritesheet = Spritesheet('img/character.png')
+        self.terrain_spritesheet = Spritesheet('img/terrain.png')
+
     def createTilemap(self):
         for i, row in enumerate(tilemap):
             for j, column in enumerate(row):
+                Ground(self,j,i)
                 if column == "B":
                     Block(self, j, i)
                 if column == "P":
