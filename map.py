57581244c09686057e8a3680cb67b40fe50b6145
jayanaka-98 <180105c@uom.lk>
2023-11-15 09:39:45 +0530

    Chat Gpt Generated Maps

--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-tilemap = [
+tilemap_2 = [
     'BBBBBBBBBBBBBBBBBBBB',
     'B..E...............B',
     'B..................B',
@@ -15,4 +15,40 @@ tilemap = [
     'B.........B........B',
     'BBBBBBBBBBBBBBBBBBBB'
 
+]
+
+tilemap_3 = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..........E.......B',
+    'B..................B',
+    'B.......BBBB.......B',
+    'B......E...........B',
+    'B..................B',
+    'B...E..............B',
+    'B..................B',
+    'B..BBBBB...........B',
+    'B.........E........B',
+    'B..................B',
+    'B............P.....B',
+    'B..................B',
+    'B.......E..........B',
+    'BBBBBBBBBBBBBBBBBBBB'
+]
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBB',
+    'B........E........B',
+    'B.BBBBBBB.BBBBBBB.B',
+    'B.B..P..B.B.....B.B',
+    'B.B.BBB.B.B.BBB.B.B',
+    'B.B.B...B...B.B.B.B',
+    'B.B.B.BBBBB.B.B.B.B',
+    'B.B.B.......B.B.B.B',
+    'B.B.B.BBB.BBB.B.B.B',
+    'B.B...B...B...B.B.B',
+    'B.BBB.BBB.BBB.BBB.B',
+    'B...B...EEE......B.',
+    'BBB.BBB.B.BBB.BBB.B',
+    'B.....B...B.....B.B',
+    'BBBBBBBBBBBBBBBBBBB'
 ]
